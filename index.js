b99221e860f9534266ee1176583da67f47cc0acf
Leonardo Rodrigues <leo.rf.dev@gmail.com>
2022-08-21 22:06:06 -0300

    feat: add prettier rules

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  proseWrap: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+};
