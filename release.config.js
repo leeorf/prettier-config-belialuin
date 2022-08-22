6f8bfdd5e1c7fd7e949bca0b127bb46ffce17026
Leonardo Rodrigues <leo.rf.dev@gmail.com>
2022-08-21 22:40:19 -0300

    chore: add tooling for automatic versioning and publishing

---
+++ release.config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md', 'README.md'],
+        message:
+          "build(release): release <%= nextRelease.version %> - <%= new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %> [skip-ci]\n\n<%= nextRelease.notes %>",
+      },
+    ],
+  ],
+};
